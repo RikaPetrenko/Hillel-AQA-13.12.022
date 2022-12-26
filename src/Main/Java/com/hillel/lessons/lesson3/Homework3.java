cc2fd6b78219e4d3919f076b83e2427a5459653d
Valeriia Kovalenko <work.valk@gmail.com>
2022-12-26 15:53:49 +0200

    Home Work 3

---
+++ src/Main/Java/com/hillel/lessons/lesson3/Homework3.java
@@ -0,0 +1,9 @@
+package com.hillel.lessons.lesson3;
+
+public class Homework3 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Woah, Homework 3 on remote repository is ready.");
+    }
+}
