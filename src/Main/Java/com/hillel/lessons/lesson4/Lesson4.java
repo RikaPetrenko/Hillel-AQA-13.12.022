95a3fadaf1e6b8a79b93059d8ba63832b0f66a0f
Valeriia Kovalenko <work.valk@gmail.com>
2022-12-27 21:08:09 +0200

    Home Work 3

---
+++ src/Main/Java/com/hillel/lessons/lesson4/Lesson4.java
@@ -0,0 +1,78 @@
+package com.hillel.lessons.lesson4;
+
+public class Lesson4 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 3;
+
+        a += 5;
+
+        /*
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+         */
+
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+        System.out.println((double) a/b);
+        System.out.println(a/ (double) b);
+        System.out.println(a % b); //залишок від ділення. 3*3 = 9; 10 - 9 = 1
+
+        int c = 10;
+        int d = 3;
+
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+        System.out.println(Math.round((int)10.7));
+
+        Math.abs(10);
+
+        Math.random();
+
+        System.out.println(Math.random());
+        System.out.println(Math.random());
+        System.out.println(Math.random());
+        System.out.println(Math.random());
+        System.out.println(Math.random());
+
+        System.out.println(Math.random() *11);
+        System.out.println((int) (Math.random() * 11));
+        System.out.println((int) (Math.random() * 11));
+        System.out.println((int) (Math.random() * 11));
+        System.out.println((int) (Math.random() * 11));
+
+        //System.out.println(15 Math.random() * 11);
+        //System.out.println(15 Math.random() * 2);
+
+        int a = 500;
+        int b = 1500;
+        int c = 3500;
+
+        int years = 10 * 12;
+
+        int 1salary10 = a * years;
+        int 2salary10 = b * years;
+        int 3salary10 = c * years;
+
+        System.out.println();
+
+
+
+
+
+
+
+
+
+    }
+}
