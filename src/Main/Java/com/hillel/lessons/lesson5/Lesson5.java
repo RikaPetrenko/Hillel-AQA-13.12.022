09adbaa0bc5feed2f7f209c2cc2ed33bbdffcc69
Valeriia Kovalenko <work.valk@gmail.com>
2022-12-30 22:29:48 +0200

    Lesson 5

---
+++ src/Main/Java/com/hillel/lessons/lesson5/Lesson5.java
@@ -0,0 +1,52 @@
+package com.hillel.lessons.lesson5;
+
+public class Lesson5 {
+    public static void main(String[] args) {
+
+
+/*
+        System.out.println( a>b );
+        System.out.println( a>=b );
+        System.out.println( a<b );
+        System.out.println( a<=b );
+        System.out.println( a==b );
+        System.out.println( a!=b );
+
+        boolean isBigger = a > b;
+        System.out.println(isBigger);
+
+        boolean booleanTrue = true;
+        System.out.println(booleanTrue);
+
+         if (a > b) {
+            System.out.println("a < b");
+        } else {
+            System.out.println("a <= b");
+
+            System.out.println(a < b && b > c);
+        if (a < b && b > c) {
+            System.out.println("Hello");
+
+ */
+
+
+            int a = 1;
+            int b = 3;
+            int c = 2;
+
+            if (a > b) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+
+                System.out.println( (a > b) ? "Yes" : "No");
+
+        }
+//(умова) , вираз1 : вираз2
+
+        }
+
+
+
+    }
+
