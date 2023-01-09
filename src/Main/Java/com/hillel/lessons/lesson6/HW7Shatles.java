3187c679eca81592be06d62eec5d7c091a53ea9c
Valeriia Kovalenko <work.valk@gmail.com>
2023-01-10 00:33:21 +0200

    Not fully finished, last batch for some reason includes 4. Resolved by including 14

--- src/Main/Java/com/hillel/lessons/lesson6/HW7Shatles.java
+++ src/Main/Java/com/hillel/lessons/lesson6/HW7Shatles.java
@@ -6,14 +6,20 @@ public class HW7Shatles {
 
 
         int Counter = 0;
-        for (int i = 1; ; i++) {
-        if (i / 10 == 4 || i / 10 == 9 || i % 10 == 4 | i % 10 == 9) {
+        int i = 0;
+        i++;
+
+        for ( ; ; i++) {
+        if (i / 10 == 4 || i / 10 == 14 || i / 10 == 9 || i % 10 == 4 | i % 10 == 9) {
             continue;
+
 
         }
 
         System.out.println(i);
         Counter++;
+
+
 
         if (Counter == 100) {
             System.out.println("Shatles quantity: " + Counter);
